ae2c9823d2849806b96c3b79fe485d241d1cc3ac
MINHHIEU-99 <bmhieu481999@gmail.com>
2024-03-20 17:17:08 +0700

    update Export Data Function

--- Javascript/Assignment/Assignment 02 (Stater)/script/data.js
+++ Javascript/Assignment/Assignment 02 (Stater)/script/data.js
@@ -1,2 +1,15 @@
 'use strict';
 
+const exportBtn = document.getElementById('export-btn');
+const importBtn = document.getElementById('import-btn');
+
+let petArr = JSON.parse(getFromStorage("petArr", '[]'));
+
+exportBtn.addEventListener('click', function saveStaticDataToFile() {
+    var blob = new Blob([JSON.stringify(petArr, null, "\t")],
+                { type: "text/plain;charset=utf-8" });
+    
+    saveAs(blob, "pets.json");
+});
+
+console.log(JSON.stringify(petArr, null, "\t"));
