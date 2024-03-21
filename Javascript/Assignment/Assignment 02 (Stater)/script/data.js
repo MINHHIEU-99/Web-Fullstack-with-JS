aa1dcfdc8594c6736dd440ad5b63fff985e6fdd3
MINHHIEU-99 <bmhieu481999@gmail.com>
2024-03-21 23:22:18 +0700

    update import and export json data

--- Javascript/Assignment/Assignment 02 (Stater)/script/data.js
+++ Javascript/Assignment/Assignment 02 (Stater)/script/data.js
@@ -2,8 +2,11 @@
 
 const exportBtn = document.getElementById('export-btn');
 const importBtn = document.getElementById('import-btn');
+const readFile = document.getElementById('input-file');
 
 let petArr = JSON.parse(getFromStorage("petArr", '[]'));
+let petArrId = JSON.parse(getFromStorage("petArrId", '[]'));
+let petAddArr;
 
 exportBtn.addEventListener('click', function saveStaticDataToFile() {
     var blob = new Blob([JSON.stringify(petArr, null, "\t")],
@@ -12,4 +15,39 @@ exportBtn.addEventListener('click', function saveStaticDataToFile() {
     saveAs(blob, "pets.json");
 });
 
-console.log(JSON.stringify(petArr, null, "\t"));
+
+async function readText(event) {
+    const file = event.target.files.item(0)
+    const text = await file.text();
+    petAddArr = JSON.parse(text);
+    // return petAddArr;
+};
+
+readFile.addEventListener('change', function (e) {
+    readText(e);
+});
+
+importBtn.addEventListener('click', function () {
+    function updatePet(item) {
+        if (petArrId.includes(item.id.toString())) {
+            
+            petArr.splice(petArrId.findIndex((j) => j == item.id), 1); 
+            petArrId.splice(petArrId.findIndex((j) => j == item.id), 1);
+            
+            petArr.unshift(item);
+            petArrId.unshift(item.id);
+        }
+        else {
+            
+            petArr.unshift(item);
+            petArrId.unshift(item.id);
+        }
+    };
+    petAddArr.forEach(updatePet);
+    console.log(petArr);
+    console.log(petArrId);
+
+    // saveToStorage('petArr', JSON.stringify(petArr));
+    // saveToStorage('petArrId', JSON.stringify(petArrId));
+});
+
