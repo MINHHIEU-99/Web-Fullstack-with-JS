8f91b43ebd2d4a42a224108bbf25d0ae32d408a4
MINHHIEU-99 <bmhieu481999@gmail.com>
2024-04-10 15:51:05 +0700

    update todoList function and fix bug currentUser undefined

--- Javascript/Assignment/Assignment 03 (Stater)/scripts/setting.js
+++ Javascript/Assignment/Assignment 03 (Stater)/scripts/setting.js
@@ -1,1 +1,5 @@
 'use strict'
+
+const sizePageInput = document.getElementById('input-page-size');
+const categoryInput = document.getElementById('input-category');
+
--- Javascript/Assignment/Assignment 03 (Stater)/scripts/todo.js
+++ Javascript/Assignment/Assignment 03 (Stater)/scripts/todo.js
@@ -8,6 +8,8 @@ const todoList = document.getElementById('todo-list');
 let currentUser = getFromStorage('CURRENT_USER');
 let todoArr = JSON.parse(getFromStorage('TOTAL_TASKS', '[]'));
 
+if (currentUser != undefined) currentUser = JSON.parse(currentUser);
+
 class Tasks {
     constructor(task, owner, isDone) {
         this.task = task;
@@ -28,29 +30,42 @@ function clearInput() {
 todoList.addEventListener('click', function (e) {
     // Change the state of task completed and not completed
     e.target.classList.toggle('checked');
+    // console.log(e.target);
 
-    // Get name of target task 
-    const contentTask = e.target.parentElement.textContent;
-    const taskName = contentTask.split('').slice(0, -1).join('');
+    function getTaskName(n) {
+        const contentTask = n.textContent;
+        return contentTask.split('').slice(0, -1).join('');
+    }
 
-    // Get index of target task 
-    const index = todoArr.findIndex(e => e.task == taskName);
-    
-
-    if (e.target.classList.contains('close')) {
+    if (e.target.classList.contains('close')) {        
         alert('Are you sure ?');
+        // console.log(e.target);
+
+        // Get name of target task
+        const taskName = getTaskName(e.target.parentNode);
+
+        // Get index of target task 
+        const index = todoArr.findIndex(e => e.task == taskName);
 
         // Delete task
         todoArr.splice(index, 1);
-        // Save to local storage
-        saveToStorage('TOTAL_TASKS', JSON.stringify(todoArr));
 
         // Re-render todoArr
         renderTodoList(currentUser, todoArr);
     }
     else {
-        todoArr[index].isDone = this.classList.contains('checked') ? true : false;
+        const taskName = getTaskName(e.target);
+
+        const index = todoArr.findIndex(e => e.task == taskName);
+
+        // Change instance isDone of target task object
+        todoArr[index].isDone = e.target.classList.contains('checked') ? true : false;
     }
+
+    // Save to local storage
+    saveToStorage('TOTAL_TASKS', JSON.stringify(todoArr));
+
+
 });
 
 addBtn.addEventListener('click', function () {
@@ -58,7 +73,7 @@ addBtn.addEventListener('click', function () {
         alert('Please log in your account');
         clearInput();
     } else {
-        currentUser = JSON.parse(currentUser);
+        // currentUser = JSON.parse(currentUser);
         console.log(1);
         const mission = {
             task: taskInput.value,
@@ -87,20 +102,29 @@ const renderTodoList = function (currentUser, taskArr) {
     if (currentUser == undefined) todoList.innerHTML = '';
     else {
         todoList.innerHTML = '';
-        currentUser = JSON.parse(currentUser);
         taskArr
             .filter(e => e.owner == currentUser.userName)
-            .forEach(element => {
-                todoList.insertAdjacentHTML(
-                    'beforeend',
-                    `<li>${element.task}<span class="close">×</span></li>`
-                );
+            .forEach(element => {                
+                if (element.isDone) {
+                    todoList.insertAdjacentHTML(
+                        'beforeend',
+                        `<li class="checked">${element.task}<span class="close">×</span></li>`
+                    );
+                }
+                else {
+                    todoList.insertAdjacentHTML(
+                        'beforeend',
+                        `<li>${element.task}<span class="close">×</span></li>`
+                    );
+                }
             });
     }
 };
 
-renderTodoList(currentUser, todoArr);
+
+// currentUser = JSON.parse(currentUser);
 console.log(currentUser);
 console.log(todoArr);
 
+renderTodoList(currentUser, todoArr);
 
