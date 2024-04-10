e2e119cbefe2f62176da7b161e0bd5b2abd76132
MINHHIEU-99 <bmhieu481999@gmail.com>
2024-04-10 10:23:02 +0700

    add todoList function

--- Javascript/Assignment/Assignment 03 (Stater)/scripts/news.js
+++ Javascript/Assignment/Assignment 03 (Stater)/scripts/news.js
@@ -1,1 +1,97 @@
-'use strict'
+'use strict';
+
+const newsContainer = document.getElementById('news-container');
+const prevBtn = document.getElementById('btn-prev');
+const nextBtn = document.getElementById('btn-next');
+const pageNum = document.getElementById('page-num');
+
+const apiKey = '445e0c7967ac4ebb82ef54528388a7f0';
+
+let curPage = parseInt(pageNum.textContent);
+const pageSize = 5;
+
+const init = function () {
+    getNews('us', curPage, pageSize);
+};
+
+// Change state of button when num of page has changed
+function changeBtn(curPage, totalResult, pageSize) {
+    if (curPage === 1) {
+        prevBtn.style.display = 'none';
+        // console.log(1);
+        return;
+    }
+    if (curPage >= totalResult / pageSize) {
+        nextBtn.style.display = 'none';
+        return;
+    } else {
+        console.log(curPage);
+        prevBtn.style.display = 'block';
+        nextBtn.style.display = 'block';
+    }
+}
+
+// Go to next or previous page
+const prevPage = function () {
+    curPage -= 1;
+    pageNum.textContent = curPage;
+    getNews('us', curPage, pageSize);
+};
+
+const nextPage = function () {
+    curPage += 1;
+    pageNum.textContent = curPage;
+    // changeBtn(curPage, 36, 5);
+    getNews('us', curPage, pageSize);
+};
+
+// Render news in one page
+const renderNews = function (img_path, title, content, url) {
+    const html = `
+        <div class="card flex-row flex-wrap">
+            <div class="card mb-3" style="">
+                <div class="row no-gutters">
+                    <div class="col-md-4">
+                        <img src="${img_path}"
+                            class="card-img"
+                            alt="${title}">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h5 class="card-title">${title}</h5>
+                            <p class="card-text">${content}</p>
+                            <a href="${url}"
+                                class="btn btn-primary">View</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>`;
+    newsContainer.insertAdjacentHTML('beforeend', html);
+};
+
+// get news through API
+async function getNews(country, page, pageSize) {
+    try {
+        const resNews = await fetch(
+            `https://newsapi.org/v2/top-headlines?country=${country}&page=${page}&pageSize=${pageSize}&apiKey=445e0c7967ac4ebb82ef54528388a7f0`
+        );
+        if (!resNews.ok) throw new Error('Problem with getting news');
+
+        const data = await resNews.json();
+        console.log(data.articles, data.totalResults);
+
+        newsContainer.innerHTML = '';
+        await changeBtn(page, data.totalResults, pageSize);
+        data.articles.forEach(e => {
+            renderNews(e.urlToImage, e.title, e.description, e.url);
+        });
+    } catch (err) {
+        console.error(err.message);
+    }
+}
+
+///////////////////////////////
+init();
+prevBtn.addEventListener('click', prevPage);
+nextBtn.addEventListener('click', nextPage);
--- Javascript/Assignment/Assignment 03 (Stater)/scripts/todo.js
+++ Javascript/Assignment/Assignment 03 (Stater)/scripts/todo.js
@@ -1,1 +1,106 @@
-'use strict'
+'use strict';
+
+const taskInput = document.getElementById('input-task');
+const addBtn = document.getElementById('btn-add');
+const todoList = document.getElementById('todo-list');
+
+
+let currentUser = getFromStorage('CURRENT_USER');
+let todoArr = JSON.parse(getFromStorage('TOTAL_TASKS', '[]'));
+
+class Tasks {
+    constructor(task, owner, isDone) {
+        this.task = task;
+        this.owner = owner;
+        this.isDone = isDone;
+    }
+}
+
+function parseTask(taskData) {
+    const task = new Tasks(taskData.task, taskData.owner, taskData.isDone);
+    return task;
+}
+
+function clearInput() {
+    taskInput.value = '';
+}
+
+todoList.addEventListener('click', function (e) {
+    // Change the state of task completed and not completed
+    e.target.classList.toggle('checked');
+
+    // Get name of target task 
+    const contentTask = e.target.parentElement.textContent;
+    const taskName = contentTask.split('').slice(0, -1).join('');
+
+    // Get index of target task 
+    const index = todoArr.findIndex(e => e.task == taskName);
+    
+
+    if (e.target.classList.contains('close')) {
+        alert('Are you sure ?');
+
+        // Delete task
+        todoArr.splice(index, 1);
+        // Save to local storage
+        saveToStorage('TOTAL_TASKS', JSON.stringify(todoArr));
+
+        // Re-render todoArr
+        renderTodoList(currentUser, todoArr);
+    }
+    else {
+        todoArr[index].isDone = this.classList.contains('checked') ? true : false;
+    }
+});
+
+addBtn.addEventListener('click', function () {
+    if (currentUser == undefined) {
+        alert('Please log in your account');
+        clearInput();
+    } else {
+        currentUser = JSON.parse(currentUser);
+        console.log(1);
+        const mission = {
+            task: taskInput.value,
+            owner: currentUser.userName,
+            isDone: false,
+        };
+        function validateData(data) {
+            if (data.task == '') {
+                alert('Title of task must be filled');
+                return false;
+            } else return 1;
+        }
+        if (validateData(mission)) {
+            const newTask = parseTask(mission);
+            todoArr.push(newTask);
+            saveToStorage('TOTAL_TASKS', JSON.stringify(todoArr));
+            renderTodoList(currentUser, todoArr);
+            console.log(todoArr);
+
+            clearInput();
+        }
+    }
+});
+
+const renderTodoList = function (currentUser, taskArr) {
+    if (currentUser == undefined) todoList.innerHTML = '';
+    else {
+        todoList.innerHTML = '';
+        currentUser = JSON.parse(currentUser);
+        taskArr
+            .filter(e => e.owner == currentUser.userName)
+            .forEach(element => {
+                todoList.insertAdjacentHTML(
+                    'beforeend',
+                    `<li>${element.task}<span class="close">×</span></li>`
+                );
+            });
+    }
+};
+
+renderTodoList(currentUser, todoArr);
+console.log(currentUser);
+console.log(todoArr);
+
+
