316e123b4c51d15812d228dddc5580500536a0b5
MINHHIEU-99 <bmhieu481999@gmail.com>
2024-05-08 13:50:46 +0700

    add MVC tutorial

---
+++ NodeJs/Learning/Basics/nodejs-complete-guide/controllers/products.js
@@ -0,0 +1,32 @@
+const Product = require('../models/product');
+
+exports.getAddProduct = (req, res, next) => {
+    // res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+    res.render('add-product', {
+        pageTitle: 'Add Product',
+        path: '/admin/add-product',
+        activeAddProduct: true,
+        productCSS: true,
+        formsCSS: true
+    });
+};
+
+exports.postAddProduct = (req, res, next) => {
+    const product = new Product(req.body.title);
+    product.save();
+    res.redirect('/');
+};
+
+exports.getProducts = (req, res, next) => {
+    Product.fetchAll((products) => {
+        res.render('shop', {
+            prods: products,
+            pageTitle: 'My Shop',
+            path: '/',
+            hasProducts: products.length > 0,
+            activeShop: true,
+            productCSS: true
+        });
+    });
+    
+}
